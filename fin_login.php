2913ea33a6c42cbadd47ae9d1041bbe40b0fbc6f
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-04-29 17:17:17 -0400

    Add files via upload

---
+++ fin_login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>SaleEstate Login Page</title>
+    <link href="fin_style.css" rel="stylesheet">
+</head>
+
+<body>
+    <form action="" method="post" name="Login_Form">
+        <table width="400" height="350" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+            <tr>
+                <td colspan="2" align="left">
+                    <h2>Login to SaleEstate</h2>
+                </td>
+            </tr>
+            <tr>
+                <td><input name="Username" type="text" class="Input" placeholder="Username" size="30"></td>
+            </tr>
+            <tr>
+                
+                <td><input name="Password" type="password" class="Input" placeholder="Password" size="30"></td>
+            </tr>
+            <tr>
+             
+                <td><input name="Submit" type="submit" value="Signup" class="Button3"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
+
+</html>
