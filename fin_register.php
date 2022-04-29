3921f57653b8bcd716aa69bca96483efb792bfe2
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-04-29 17:17:00 -0400

    Add files via upload

---
+++ fin_register.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>SaleEstate Register Page</title>
+    <link href="fin_style.css" rel="stylesheet">
+</head>
+
+<body>
+    <form action="" method="post" name="Login_Form">
+        <table width="400" height="350" border="0" align="center" cellpadding="5" cellspacing="1" class="Table">
+            <tr>
+                <td colspan="2" align="left">
+                    <h2>Register for SaleEstate</h2>
+                </td>
+            </tr>
+            <tr>
+                <td><input name="Username" type="text" class="Input" placeholder="Username" size="30"></td>
+            </tr>
+            <tr>
+                
+                <td><input name="Password" type="password" class="Input" placeholder="Password" size="30"></td>
+            </tr>
+            <tr>
+             
+                <td><input name="Submit" type="submit" value="Signup" class="Button3"></td>
+            </tr>
+            <tr>
+              
+                <td>Already have an account? <a href="fin_login.php">Login here!</a></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
