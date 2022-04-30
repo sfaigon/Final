648b8a1b71e0e3553a55906a30d3d37c9c014de8
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-04-30 13:34:11 -0400

    Add files via upload

---
+++ fin_about.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html>
+
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>About SaleEstate</title>
+	<link href="fin_style.css" rel="stylesheet">
+</head>
+<body style="background-color:white">
+
+    <header>
+        <div class="inner">
+            <div class="logo">
+                <div>
+                    <img src="fin_logo.png" alt="SaleEstate logo" width="170" height="60">
+                </div>
+            </div>
+            <nav>
+			    
+                <li><span><a href="fin_index.php" class="button">Back</a></span></li>
+		    </nav>
+	    </div>
+	</header>
+
+<main>
+		<h1>ABOUT <span style="color:#33cc33">Sale</span><span style="color:#d9d9d9">Estate</span></h1>
+		<p>Here at SaleEstate, we strive to market the best home options to you or provide a way for you to sell your properties with ease.</p>
+</main>
+</body>
+</html>
