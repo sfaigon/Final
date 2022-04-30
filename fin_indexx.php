22df7c2737e929e12d1a2f4e22bb006b568a3303
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-04-30 13:33:26 -0400

    Add files via upload

---
+++ fin_indexx.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html>
+
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>Seller HomePage</title>
+	<link href="fin_style.css" rel="stylesheet">
+</head>
+<body style="background-color:white">
+
+    <header>
+        <div class="inner">
+            <div class="logo">
+                <div>
+                    <img src="fin_logo.png" alt="SaleEstate logo" width="170" height="60">
+                </div>
+            </div>
+            <nav>
+			    <li><span>Hello Seller <?=$_POST['Username']?></span></li>
+                <li><span><a href="fin_register.php" class="button">Logout</a></span></li>
+		    </nav>
+	    </div>
+	</header>
+
+<main>
+		<h1>SELLER HOMEPAGE</h1>
+		
+</main>
+</body>
+</html>
