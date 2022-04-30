e4e9c67e47bdf49fef111416253da9185dcbb3f3
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-04-30 13:32:05 -0400

    Update fin_login.php

--- fin_login.php
+++ fin_login.php
@@ -2,17 +2,17 @@
 <html lang="en">
 
 <head>
-    <title>SaleEstate Login Page</title>
+    <title>SaleEstate Login</title>
     <link href="fin_style.css" rel="stylesheet">
 </head>
 
 <body>
-    <form action="fin_index.php" method="post" name="Login_Form">
+    <form action="fin_index.php" method="post">
         <table width="400" height="350" border="0" align="center" cellpadding="20" cellspacing="1" class="Table">
             <tr>
                 <td colspan="2" align="left">
-                    <img src="fin_logo.png" width="400" height="180">
-                    <h2>Login</h2>
+					<img src="fin_logo.png" width="400" height="180">
+					<h2>Login</h2>
                 </td>
             </tr>
             <tr>
@@ -24,7 +24,7 @@
             </tr>
             <tr>
              
-                <td><input name="Submit" type="submit" value="Login" class="Button3"></td>
+                <td><input name="Submit" type="submit" value="Buyer Login" class="Button3"><input name="Submit" type="submit" value="Seller Login" class="Button3" formaction="fin_indexx.php"></td>
             </tr>
         </table>
     </form>
