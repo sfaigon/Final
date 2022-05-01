d3ae4ce3bfd546533a3988a3428ff2e96b043649
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-05-01 00:12:39 -0400

    Update fin_indexx.php

--- mypart/fin_indexx.php
+++ mypart/fin_indexx.php
@@ -7,25 +7,22 @@
 	<title>Seller HomePage</title>
 	<link href="fin_style.css" rel="stylesheet">
 </head>
-<body style="background-color:white">
-
-    <header>
-        <div class="inner">
-            <div class="logo">
-                <div>
-                    <a href="fin_aboutt.php" title="About SaleEstate"><img src="fin_logo.png" alt="SaleEstate logo" width="170" height="60"></a>
-                </div>
-            </div>
-            <nav>
-			    <li><span>Hello Seller <?=$_POST['Username']?></span></li>
-                <li><span><a href="fin_register.php" class="button">Logout</a></span></li>
-		    </nav>
-	    </div>
-	</header>
-
-<main>
-		<h1>SELLER HOMEPAGE</h1>
-		
-</main>
+<body>
+		<div class="center">
+	<img src="fin_logo.png" class="center">
+</div>
+<div class="center">
+<div>
+	<p><a href="fin_addhome.php"> Add a Home to your listings</a> </p>
+	No records matching your query were found.</div>
+<div>
+	<p>Want to Delete a listing?</p>
+	<form action="" method="post">
+		<h3>Home Id's</h3>
+		<p>Enter Home ID: <input type="number" name="id"></p>
+		<input type="submit" id="deleteHouse" value="submit">
+	</form>
+</div>
+</div>
 </body>
 </html>
