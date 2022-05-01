7ba753f26adf712187a7d54ec114984d906aef2e
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-05-01 00:16:18 -0400

    Add files via upload

---
+++ mypart/fin_addseller.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "aagbe1";
+$password = "aagbe1";
+$dbname = "aagbe1";
+
+//connect to existing database
+$db=mysqli_connect($servername,$username,$password,$dbname);
+
+//define user-inputted username/password
+$id=$_POST['s_id'];
+$Username=$_POST['Username'];
+$Password=$_POST['Password'];
+
+//retrieves user/pass
+$sql = "INSERT INTO seller_ids (id,Username,Password) VALUES ('$s_id','$Username','$Password')";
+
+$db->close();
+?>
