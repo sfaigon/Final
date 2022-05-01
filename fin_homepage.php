4eacffde25b586b63ae77ab19b61e7bdd3c1f5d1
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-05-01 15:16:11 -0400

    Add files via upload

---
+++ fin_homepage.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+
+<head lang="en">
+	<meta charset="UTF-8">
+	<title>SaleEstate HomePage</title>
+	<link href="fin_style3.css" rel="stylesheet">
+</head>
+<body>
+<div class="center">
+      <img src="fin_logo.png" class="center">  
+</div>
+<div class="center">
+		<div><h1>About SaleEstate</h1></div>
+		<div><p>Here at SaleEstate, we strive to market the best home options to you or provide a way for you to sell your properties with ease.</p></div>
+		<div><a href="fin_buyerregister.php">Buyer Register</a></div>
+		<div><a href="fin_sellerregister.php">Seller Register</a></div>
+</div>
+</body>
+</html>
