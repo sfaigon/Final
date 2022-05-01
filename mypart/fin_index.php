60278a3ddd6f7209f5c6cd219d453f002a64984a
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-05-01 00:12:09 -0400

    Update fin_index.php

--- mypart/fin_index.php
+++ mypart/fin_index.php
@@ -7,25 +7,13 @@
 	<title>Buyer HomePage</title>
 	<link href="fin_style.css" rel="stylesheet">
 </head>
-<body style="background-color:white">
-
-    <header>
-        <div class="inner">
-            <div class="logo">
-                <div>
-                    <a href="fin_about.php" title="About SaleEstate"><img src="fin_logo.png" alt="SaleEstate logo" width="170" height="60"></a>
-                </div>
-            </div>
-            <nav>
-			    <li><span>Hello Buyer <?=$_POST['Username']?></span></li>
-                <li><span><a href="fin_register.php" class="button">Logout</a></span></li>
-		    </nav>
-	    </div>
-	</header>
-
-<main>
-		<h1>BUYER HOMEPAGE</h1>
-		<a href="ViewHomes.php">View Homes</a>
-</main>
+<body>
+<div class="center">
+      <img src="fin_logo.png" class="center">  
+</div>
+<div class="center">
+		<div><h1>BUYER HOMEPAGE</h1></div>
+		<div><a href="ViewHomes.php">View Homes</a></div>
+</div>
 </body>
 </html>
