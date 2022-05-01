c2dd006299a036b0913bde1e5097443efce11218
aagbe1 <98627137+aagbe1@users.noreply.github.com>
2022-05-01 12:08:45 -0400

    Update fin_about.php

--- fin_about.php
+++ fin_about.php
@@ -5,7 +5,7 @@
 <head lang="en">
 	<meta charset="UTF-8">
 	<title>About SaleEstate</title>
-	<link href="fin_style.css" rel="stylesheet">
+	<link href="fin_style3.css" rel="stylesheet">
 </head>
 <body>
 <div class="center">
